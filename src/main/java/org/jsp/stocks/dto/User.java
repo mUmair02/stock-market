f68c4549a11bce5b288543dfd6f567aef1ddb8dc
umair <md006umair@gmail.com>
2025-03-30 12:15:18 +0530

    day-02

--- src/main/java/org/jsp/stocks/dto/User.java
+++ src/main/java/org/jsp/stocks/dto/User.java
@@ -1,11 +1,46 @@
 package org.jsp.stocks.dto;
 
+import java.time.LocalDate;
+
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@ToString
 public class User {
-	private String username;
+	@Size(min=2,max=15, message = "*it should be between 3 to 15 characters")
+	private String name;
+	
+	@Email(message = "*It should be a valid email")
+	@NotEmpty(message = "*It is required")
+	private String email;
+	
+	
+	@DecimalMin(value = "6000000000", message = "*It should be proper Mobile Number")
+	@DecimalMax(value = "9999999999", message = "*It should be proper Mobile Number")
+	private long mobile;
+	
+	@Past(message = "*Enter proper DOB")
+	@NotNull(message = "*It is required")
+	private LocalDate dob;
+	
+	
+	@Pattern(regexp = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",message = "*  It should be One Uppercase, One Lowercase, One Number, One Special Chrecter,Minium 8 charecters")
 	private String password;
+	
+	
+	@Pattern(regexp = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",message = "*  It should be One Uppercase, One Lowercase, One Number, One Special Chrecter,Minium 8 charecters")
+	private String confirmPassword;
 }
+
--- src/main/java/org/jsp/stocks/service/StockService.java
+++ src/main/java/org/jsp/stocks/service/StockService.java
@@ -1,11 +1,17 @@
 package org.jsp.stocks.service;
 
 import org.jsp.stocks.dto.User;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 public interface StockService {
 
-	String login(User user, HttpSession session);
+	
+
+
 
 }
+
