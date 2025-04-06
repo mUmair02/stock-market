85cbf0367a267548aa3b2a252dfadb26e9702ebe
umair <md006umair@gmail.com>
2025-04-06 17:13:21 +0530

    day-03(otp verification)

--- src/main/java/org/jsp/stocks/controller/StockController.java
+++ src/main/java/org/jsp/stocks/controller/StockController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,8 +44,9 @@ public class StockController {
 	public String Register(@ModelAttribute @Valid User user,BindingResult result) {
 		return service.register(user,result);
 	}
-	@GetMapping("/otp")
-	public String loadOtpPage() {
+	@GetMapping("/otp/{id}")
+	public String loadOtpPage(@PathVariable int id,Model model) {
+		model.addAttribute("id", id);
 		return "otp.html";
 	}
 	@PostMapping("/otp")
